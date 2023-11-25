cd1777c1824fd4741ecc74962c2a18ab8f296ed8
Fernando Caponetto <fernando.caponetto@gmail.com>
2023-11-25 13:19:57 +0000

    submissions.ts: modified the query structure to accommodate more information for the results data

--- src/graphql/submissions.ts
+++ src/graphql/submissions.ts
@@ -2,7 +2,22 @@ export default `
 query ($offset: Int!, $limit: Int!, $slug: String) {
     submissionList(offset: $offset, limit: $limit, questionSlug: $slug) {
         hasNext
-        submissions { id lang time timestamp statusDisplay runtime url isPending title memory titleSlug }
+        submissions {
+          id
+          title
+          titleSlug
+          status
+          statusDisplay
+          lang
+          langName
+          runtime
+          timestamp
+          url
+          isPending
+          memory
+          hasNotes
+          notes
+        }
     }
 }
 `;
